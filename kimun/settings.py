350afbecdb7d06330cd147c44979a1ac55c0abb6
SebaGRT <coke2014@outlook.cl>
2026-05-04 17:05:33 -0400

    test: use FileSystemStorage during tests to avoid Supabase dependency

--- kimun/settings.py
+++ kimun/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+import sys
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -174,3 +179,6 @@ if not DEBUG:
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+
+if 'test' in sys.argv:
+    STORAGES['default']['BACKEND'] = 'django.core.files.storage.FileSystemStorage'
